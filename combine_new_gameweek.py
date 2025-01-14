75607b450e409c44982163d9b93e399e1c4ecfaa
Jakob Fahl <s184419@student.dtu.dk>
2025-01-14 12:42:53 +0000

    new data

--- combine_new_gameweek.py
+++ combine_new_gameweek.py
@@ -245,7 +245,7 @@ pos_mapping = {'GK': 1,
 
 def main():
     # Parse to get newest data
-    # parse_data()
+    parse_data()
 
     # Merge newest season FPL and Understat data
     df_24 = merge_season('2024-25', player_mapping_24)
